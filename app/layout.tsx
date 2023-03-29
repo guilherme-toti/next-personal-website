e5b48fa84d211f6a4dac282af7e8f4bf79a72bab
Guilherme Toti <guilhermetoti@gmail.com>
2023-03-29 00:17:16 -0300

    Add openGraph metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,26 @@
 import './globals.css'
 
+const title = 'Guilherme Toti - Lead Software Engineer'
+const description = 'A full-stack software developer with extensive experience in Python and React.JS'
 export const metadata = {
-    title: 'Guilherme Toti - Lead Software Engineer',
-    description: 'A full-stack software developer with extensive experience in Python and React.JS',
+    title,
+    description,
+    openGraph: {
+        title,
+        description,
+        url: 'https://guilhermetoti.com',
+        siteName: 'Guilherme Toti',
+        images: [
+            {
+                url: 'https://www.guilhermetoti.com/guilherme-toti-portrait.webp',
+                width: 908,
+                height: 908,
+                alt: 'Guilherme Toti Profile Picture',
+            },
+        ],
+        locale: 'en-US',
+        type: 'website',
+    },
 }
 
 export default function RootLayout({
